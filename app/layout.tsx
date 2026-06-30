9fb909ec4a873cf9a614d348d408b23e80d0af0f
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-06-30 20:54:52 +0300

    Add global layout with header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="uk" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
-        <Providers>{children}</Providers>
+        <Header />
+        <TanStackProvider>{children}</TanStackProvider>
+        <Footer />
       </body>
     </html>
   );
